c073c841909ba17210919fe93cf13cc9b3ef50ef
Simon Vargas <sjvargas01@gmail.com>
2020-10-04 17:08:00 -0400

    updated tasks_screen.dart

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -6,32 +6,96 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-          CircleAvatar(
-            child: Icon(
-              Icons.list,
-              size: 30.0,
-              color: Colors.lightBlueAccent,
-            ),
-            backgroundColor: Colors.white,
-            radius: 30.0,
-          ),
-          SizedBox(height: 10.0),
-          Text(
-              'Todo',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+          padding: EdgeInsets.only(
+              top: 60.0,
+              left: 30.0,
+              right: 30.0,
+              bottom: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CircleAvatar(
+                child: Icon(
+                  Icons.list,
+                  size: 30.0,
+                  color: Colors.lightBlueAccent,
+                ),
+                backgroundColor: Colors.white,
+                radius: 30.0,
               ),
+              SizedBox(height: 10.0),
+              Text(
+                'Todo',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700
+                ),
+              ),
+              Text(
+                '12 Tasks',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+
+            ],
           ),
-        ],
+        ),
+       Expanded(
+         child:  Container(
+          decoration: BoxDecoration(
+           color: Colors.white,
+            borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+          ),
+           child: ListView(
+             children: <Widget>[
+               ListTile(
+                 title: Text('This is a task.'),
+                 trailing: Checkbox(value: false),
+               ),
+               ListTile(
+                 title: Text('This is a task.'),
+                 trailing: Checkbox(value: false),
+               ),
+               ListTile(
+                 title: Text('This is a task.'),
+                 trailing: Checkbox(value: false),
+               ),
+               ListTile(
+                 title: Text('This is a task.'),
+                 trailing: Checkbox(value: false),
+               ),
+               ListTile(
+                 title: Text('This is a task.'),
+                 trailing: Checkbox(value: false),
+               ),
+               ListTile(
+                 title: Text('This is a task.'),
+                 trailing: Checkbox(value: false),
+               )
+             ],
+           ),
         ),
       ),
+        ],
+
+      ),
+
     );
   }
 }
+
+
